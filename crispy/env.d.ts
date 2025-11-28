30f966e9503ee4116dec60ed83ac710bb5c9d33e
Ismail Tabtabai <ismail@tabtabai.org>
2025-11-28 21:28:13 +0300

    vue/vite/frappeui setup changes.

--- crispy/env.d.ts
+++ crispy/env.d.ts
@@ -1,8 +1,7 @@
 /// <reference types="vite/client" />
 
 declare module "*.vue" {
-  import type { DefineComponent } from "vue"
-  const component: DefineComponent<{}, {}, any>
+  const component: import("vue").DefineComponent
   export default component
 }
 
